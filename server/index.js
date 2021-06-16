20135edac1da3929592edf20c52e62a5761df354
cmur85 <39341418+cmurestudillos@users.noreply.github.com>
2021-06-16 09:52:50 +0200

    Deploy de la Aplicacion: Parte III

--- server/index.js
+++ server/index.js
@@ -6,12 +6,12 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 // Uso de middleware
-app.use(express.static(__dirname + 'client'));
+app.use(express.static('client'));
 
 //Creacion de una ruta de Testing del servicio
-app.get('/hola-mundo', (req, res) => {
-    res.status(200).send('Hola Mundo');
-});
+// app.get('/hola-mundo', (req, res) => {
+//     res.status(200).send('Hola Mundo');
+// });
 
 var message = [{
     id: 1,
@@ -22,12 +22,9 @@ var message = [{
 //Recibir conexiones del cliente
 io.on('connection', (socket) => {
     console.log('El Equipo con IP '+ socket.handshake.address+' se ha conectado.');
-
     socket.emit('messages', message);
-
     socket.on('add-message', (data) => {
         message.push(data);
-
         io.sockets.emit('messages', message);
     });
 });
